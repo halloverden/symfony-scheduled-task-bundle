dd3c1876b0cacf2a45cafd59734fbd82efe5f231
marco-giannetti <marco@halloverden.no>
2021-05-28 14:30:50 +0200

    FIX: removed config file autoconfiguration

--- DependencyInjection/HalloVerdenScheduledTaskExtension.php
+++ DependencyInjection/HalloVerdenScheduledTaskExtension.php
@@ -5,16 +5,12 @@ namespace HalloVerden\ScheduledTaskBundle\DependencyInjection;
 
 
 use Exception;
-use HalloVerden\ScheduledTaskBundle\Interfaces\ScheduledTaskInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Yaml\Yaml;
 
-class HalloVerdenScheduledTaskExtension extends Extension implements PrependExtensionInterface {
-  const FRAMEWORK = 'framework';
+class HalloVerdenScheduledTaskExtension extends Extension {
 
   /**
    * @inheritDoc
@@ -23,12 +19,6 @@ class HalloVerdenScheduledTaskExtension extends Extension implements PrependExte
   public function load(array $configs, ContainerBuilder $container) {
     $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
     $loader->load('services.yaml');
-    $container->registerForAutoconfiguration(ScheduledTaskInterface::class)
-      ->addTag('hallo_verden_scheduler.scheduled_task');
   }
 
-  public function prepend(ContainerBuilder $container) {
-    $value = Yaml::parseFile(__DIR__ . '/../Resources/config/hallo_verden_scheduler.yaml');
-    $container->prependExtensionConfig(self::FRAMEWORK, $value[self::FRAMEWORK]);
-  }
 }
